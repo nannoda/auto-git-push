e3c9bab34ae0c97bf994982e14c26d8950b2bc1b
NaN Noda <114621472+NaNnoda@users.noreply.github.com>
2023-03-30 09:53:45 -0500

    auto-push

--- auto-push.py
+++ auto-push.py
@@ -1,4 +1,10 @@
 import asyncio
+LOGGING = True
+
+
+def log(msg)->None:
+    if LOGGING:
+        print(msg)
 
 async def commit(path:str = "."):
     print(f"Adding {path}...")
@@ -8,33 +14,51 @@ async def commit(path:str = "."):
         stderr=asyncio.subprocess.PIPE
     )
     stdout, stderr = await proc.communicate()
-    print(f"[stdout] {stdout.decode()}")
-    print(f"[stderr] {stderr.decode()}")
-    print("Commiting...")
+    log(f"[stdout] {stdout.decode()}")
+    log(f"[stderr] {stderr.decode()}")
+    log("Commiting...")
     proc = await asyncio.create_subprocess_exec(
         "git", "commit", "-m", "auto-push",
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE
     )
     stdout, stderr = await proc.communicate()
-    print(f"[stdout] {stdout.decode()}")
-    print(f"[stderr] {stderr.decode()}")
-    print("Pushing...")
+    log(f"[stdout] {stdout.decode()}")
+    log(f"[stderr] {stderr.decode()}")
+    log("Pushing...")
     proc = await asyncio.create_subprocess_exec(
         "git", "push",
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE
     )
     stdout, stderr = await proc.communicate()
-    print(f"[stdout] {stdout.decode()}")
-    print(f"[stderr] {stderr.decode()}")
-    print("Done")
+    log(f"[stdout] {stdout.decode()}")
+    log(f"[stderr] {stderr.decode()}")
+    log("Done")
 
 
 
 
 async def main():
-    print("Starting...")
+    # getting args using argparse
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--log", "-l", 
+                        help="log the output",
+                        type=bool,
+                        default=True)
+    parser.add_argument("--interval", "-i", 
+                        help="interval in seconds", 
+                        type=int,
+                        default=60)
+    args = parser.parse_args()
+    global LOGGING
+    LOGGING = args.log
+    log(args.interval)
+
+
+
+    log("Starting...")
     await commit()
 
 if __name__ == "__main__":
